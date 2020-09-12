14a820b809b883faefa2d41cdbfaecb28ddd448a
liangyouwen <layouwen@gmail.com>
2020-09-13 01:47:34 +0800

    书写Header组件html结构和css样式

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="javascript:">首页</a></li>
+      <li><a href="javascript:">新手入门</a></li>
+      <li><a href="javascript:">API</a></li>
+      <li><a href="javascript:">关于</a></li>
+      <li><a href="javascript:">登录</a></li>
+      <li><a href="javascript:">注册</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped lang="scss">
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    background: #5a5555;
+
+    > img {
+      max-width: 120px;
+      margin-left: 50px;
+    }
+
+    > ul {
+      display: flex;
+      > li {
+        padding: 0 15px;
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import Vue from 'vue';
+  import Header from '@/components/Header.vue'; // @ is an alias to /src
 
-export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+  export default Vue.extend({
+    name: 'Home',
+    components: {
+      Header,
+    },
+  });
 </script>
